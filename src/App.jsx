52641c4f5ecabf0ecf5eb0bd7f559f0692e04d1b
AyoubNL <gmilio@live.nl>
2024-04-26 04:58:15 +0200

    Compleet deel 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import {available} from "./constants/available.js";
 import {tvName} from "./constants/tvname.js";
 import {tvPrice} from "./constants/tvprice.js";
 import {tvSize} from "./constants/tvsize.js";
+import minus from "./assets/minus.png";
+import check from "./assets/check.png";
+
 
 
 function App() {
@@ -19,6 +22,19 @@ function App() {
         document.getElementById('tvprice').innerText = tvPrice
         document.getElementById('tvsize').textContent = tvSize
     }, []);
+
+    function mostSold (){
+        console.log('Meest verkocht eerst')
+    }
+
+    function cheap (){
+        console.log('Goedkoopste eerst')
+    }
+
+    function use (){
+        console.log('Meest geschikt voor sport eerst')
+    }
+
 
     return (<div>
             <h1>Tech it easy dashboard</h1>
@@ -38,16 +54,16 @@ function App() {
                 <span id={'tvprice'}></span>
                 <span id={'tvsize'}></span>
 
-                <span><img src="src/assets/check.png"/>wifi<img src="src/assets/minus.png"/>speech<img
-                    src="src/assets/check.png"/>hdr<img src="src/assets/check.png"/>bluetooth<img
-                    src="src/assets/minus.png"/>ambilight</span>
+                <span><img src={check}/>wifi<img src={minus}/>speech<img
+                    src={check}/>hdr<img src={check}/>bluetooth<img
+                    src={minus}/>ambilight</span>
             </div>
 
             <p>Alle tv's</p>
 
-            <button>Meest verkocht eerst</button>
-            <button>Goedkoopste eerst</button>
-            <button>Meest geschikt voor sport eerst</button>
+            <button onClick={mostSold}>Meest verkocht eerst</button>
+            <button onClick={cheap}>Goedkoopste eerst</button>
+            <button onClick={use}>Meest geschikt voor sport eerst</button>
 
 
         </div>
--- src/constants/tvsize.js
+++ src/constants/tvsize.js
@@ -1,10 +1,12 @@
 import {bestSellingTv} from "./inventory.js";
-
-export let tvSize = bestSellingTv.availableSizes.map((availablesize) =>
+let tvSizeNew = bestSellingTv.availableSizes.map((availablesize) =>
 {
     return ( availablesize+ ' inch ' + '('+  Math.round(availablesize * 2.54) + ' cm) ')
 }
 )
+
+
+export let tvSize = tvSizeNew.join(' | ')
 
 
 
